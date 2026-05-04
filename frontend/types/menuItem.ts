b874c03c81c472e4f49e120df804c4b32b58765e
Elisha Nguyen <elisha.nguyen7@gmail.com>
2026-05-04 12:47:56 -0700

    resolve conflicts with main

--- frontend/types/menuItem.ts
+++ frontend/types/menuItem.ts
@@ -1,21 +1,19 @@
 export type MenuItem = {
-  index: number;
-  item_id: number;
+  item_id: string;
   menu_item_name: string;
   category: string;
   price: number;
+  golden_ratio: number;
+  ai_description: string;
+  restaurant_name: string;
   calories: number;
   protein: number;
+  total_fat: number;
+  total_carbohydrates: number;
   dietary_fiber: number;
   sugars: number;
   sodium: number;
   cholesterol: number;
-  total_carbohydrates: number;
   potassium: number;
-  total_fat: number;
   serving_size: string;
-
-  // not in backend structure for an item but would be useful to have for frontend
-  image_url: string;
-  restaurant_name: string; 
 };
