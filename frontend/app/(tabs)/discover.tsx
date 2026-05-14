ab8bd1e87d16fecf907adc1e8c6af581074345ca
Elisha Nguyen <elisha.nguyen7@gmail.com>
2026-05-13 17:38:23 -0700

    load items on discover page

--- frontend/app/(tabs)/discover.tsx
+++ frontend/app/(tabs)/discover.tsx
@@ -2,21 +2,47 @@ import { View, Text, StyleSheet, ScrollView, ActivityIndicator } from "react-nat
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useState } from "react";
 import { useRouter, } from "expo-router";
-import { useMenu } from "../../hooks/useMenu";
+import { useUser } from "../../context/UserContext";
+import { getMinAndMaxValues } from "../../types/user";
+import { useItemSearch } from "../../hooks/useItemSearch";
 import AppHeader from "../../components/AppHeader";
 import DiscoverItemCard from "../../components/DiscoverItemCard";
 import FilterModal from "../../components/TargetModal";
 
 export default function Discover() {
+  const { user } = useUser();
   const router = useRouter();
   const insets = useSafeAreaInsets();
   const [filtersVisible, setFiltersVisible] = useState(false);
 
-  // load just one restaurant for now
-  const [restaurant] = useState("Chick-fil-A");
-  const { items } = useMenu(restaurant);
+  /* Load items based on user constraints */
+  const ranges = user ? getMinAndMaxValues(user) : null;
+  const {
+    results: items,
+    loading,
+    error,
+  } = useItemSearch(
+    ranges
+      ? {
+          price_min: ranges.minPrice,
+          price_max: ranges.maxPrice,
+
+          calories_min: ranges.minCalories,
+          calories_max: ranges.maxCalories,
+
+          protein_min: ranges.minProtein,
+          protein_max: ranges.maxProtein,
+        }
+      : {}
+  );
+
+  // display first 20
+  const displayedItems = items.slice(0, 20);
 
   const hues = [0, 130, 220, 30, 300]; 
+
+  if (loading) return <ActivityIndicator />;
+  if (error) return <Text>Failed to load items.</Text>;
 
   return (
     <View style={styles.container}>
@@ -31,7 +57,7 @@ export default function Discover() {
           paddingBottom: insets.bottom,
         }}
       >
-        {items.map((item, i) => (
+        {displayedItems.map((item, i) => (
         <DiscoverItemCard 
           key={item.item_id}
           item={item}
@@ -40,7 +66,7 @@ export default function Discover() {
             router.push({
               pathname: "/item/[restaurant]/[id]",
               params: {
-                restaurant,
+                restaurant: item.restaurant_name,
                 id: item.item_id,
               },
             })
