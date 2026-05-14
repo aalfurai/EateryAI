31e3d82c07e964240111cd9d0756a2e4e830b661
Elisha Nguyen <elisha.nguyen7@gmail.com>
2026-05-13 17:45:57 -0700

    reorganize default user, update login

--- frontend/app/(auth)/login.tsx
+++ frontend/app/(auth)/login.tsx
@@ -2,35 +2,14 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
 import { useUser } from "../../context/UserContext";
-import { User } from "../../types/user";
+import { defaultUser } from "../../data/defaultUser";
 import EatLogo from "../../assets/EatLogo";
 import Octicons from '@expo/vector-icons/Octicons';
 
-// TODO: UPDATE USER LOGIC
-const dummyUser: User = {
-  user_id: "1",
-  name: "dummyUser",
-  constraints: {
-    price: 14,
-    calories: 800,
-    protein: 20,
-    price_tol_pct: 0.2,
-    calories_tol_pct: 0.1,
-    protein_tol_pct: 0.3,
-  },
-  weights: {
-    price: 0.2,
-    calories: 0.4,
-    protein: 0.4,
-    fiber: 0,
-    sugar: 0,
-    sodium: 0,
-    drink_cal: 0,
-    addon_cal: 0,
-  },
-};
-
 export default function Login() {
+  const { setUser } = useUser();
+  setUser(defaultUser); // Set default user on login for demo purposes
+  
   const router = useRouter();
 
   return (
